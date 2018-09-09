5b730866771762d8fe33e9b2818524830eb5d4a5
toshusai <shu.saito.7d@stu.hosei.ac.jp>
2018-09-09 09:32:03 +0900

    Add TitleSceneManager.

---
+++ Assets/toshusai/Scripts/TitleSceneManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneManager : MonoBehaviour {
+
+    [SerializeField]
+    private string gameSceneName = "";
+   
+
+    public void ChangeGameScene(){
+        SceneManager.LoadSceneAsync(gameSceneName);
+    }
+}
